cb166bea1e766148108118b0d228feea4430fb5b
ash k <ashg4m3r@gmail.com>
2023-11-18 00:01:57 +0530

    first try

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+data=pd.read_csv('dat.csv')
+X=data.drop('Crops',axis=1)
+y=data['Crops']
+X_train,X_test,y_train,y_test=train_test_split(X,y,train_size=0.7,random_state=8)
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+model=SVC()
+model.fit(X_train,y_train)
+y_pred=model.predict(X_test)
+print(accuracy_score(y_test,y_pred))
+
+@app.route('/')
+def hello():
+    return "Hello"
+if __name__ == '__main__':
+    app.run(debug=True)
